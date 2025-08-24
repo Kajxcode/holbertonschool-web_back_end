9bfc0927c577a86ef25e04f0969af9e5c811da38
kajxcode <kaj@gmail.com>
2025-08-25 00:43:25 +1000

    fx

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,6 +4,7 @@ This module defines a function
 """
 from typing import Iterable, List, Tuple, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """annotation"""
     return [(i, len(i)) for i in lst]
