47c7f83319d6e7f681076ab5ce8cc6c18d3b2526
kajxcode <kaj@gmail.com>
2025-08-25 00:15:19 +1000

    add

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this module defines a function"""
+import math
+
+
+def floor(n: float) -> int:
+    """finds floor of a float"""
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""this module defines a function"""
+
+def to_str(n: float) -> str:
+    """converts float to str"""
+    return str(n)
