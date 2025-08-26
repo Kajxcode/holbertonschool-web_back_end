3b06b84976480f0de46c81e4fe7fce2e41b2ee98
kajxcode <kaj@gmail.com>
2025-08-27 00:38:02 +1000

    fx

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     for task in asyncio.as_completed(
         [wait_random(max_delay) for _ in range(n)]
-        ):
-            delay = await task
-            delays.append(delay)
+    ):
+        delay = await task
+        delays.append(delay)
     return delays
