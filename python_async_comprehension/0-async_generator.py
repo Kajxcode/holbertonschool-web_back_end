7ce4062387adcbc08bf272a65100df63989fb8f3
kajxcode <kaj@gmail.com>
2025-08-27 21:22:59 +1000

    fx

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 """this module defines a function"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
-
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """function"""
     for _ in range(10):
         await asyncio.sleep(1)
