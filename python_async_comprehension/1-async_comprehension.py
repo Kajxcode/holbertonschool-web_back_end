7c58edcbce2a045063d00716d8acb2858e784603
kajxcode <kaj@gmail.com>
2025-08-27 21:40:37 +1000

    fx

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() -> List[float]
+async def async_comprehension() -> List[float]:
     """function"""
     numbers = []
     
