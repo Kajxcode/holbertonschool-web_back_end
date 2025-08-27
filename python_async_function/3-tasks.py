d877789ac54713ed6c9ab8f541e4fa1b964b01cf
kajxcode <kaj@gmail.com>
2025-08-27 21:11:36 +1000

    fx

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module defines a function"""
+import time
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """"function"""
+    return asyncio.create_task(wait_random(max_delay))
---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""this module defines a function"""
+from typing import List
+import asyncio
+task_wait_random = __import__('0-basic_async_syntax').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """"function"""
+    delays = []
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
