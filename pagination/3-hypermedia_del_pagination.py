a103c14c4fea0490b899d7efbaaf0e7ad038c3f7
kajxcode <kaj@gmail.com>
2025-08-28 22:07:37 +1000

    fx

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """cursor pager"""
         data = self.dataset()
         total_items = len(data)
 
