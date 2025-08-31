627f000b364297de2d0baac19d93f8c7153b2ac6
kajxcode <kaj@gmail.com>
2025-08-31 18:25:00 +1000

    fx

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@ from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
     """inserts new document in collection"""
-    insert = mongo_collection.insert(kwargs)
-    return insert.inserted_id
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
