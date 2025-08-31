3507683f338cff1d0bddac9eb5ca5aafeee2198c
kajxcode <kaj@gmail.com>
2025-08-31 18:27:05 +1000

    fx

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """inserts new document in collection"""
-    new_topic = mongo_collection.updateMany(
+    new_topic = mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
