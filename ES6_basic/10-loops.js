758c60b278b4dd50be60b371876f88a797417208
kajxcode <kaj@gmail.com>
2025-09-02 21:52:05 +1000

    fx

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [i, value] of array.entries()) {
-    array[i] = appendString + value;
-  }
-  return array;
+  return array.map(value => appendString + value);
 }
