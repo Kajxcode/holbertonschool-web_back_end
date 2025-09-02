709ae4f4561d9a9b612ad381e4b0d6b8795abdc5
kajxcode <kaj@gmail.com>
2025-09-02 12:03:48 +1000

    fx

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-  }
+
+    (function() {
+      var task = true;
+      var task2 = false;
+  })();
+}
 
   return [task, task2];
 }
