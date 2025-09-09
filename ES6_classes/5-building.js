d49ec64671aa67627db1645db25e1bd95629459a
kajxcode <kaj@gmail.com>
2025-09-09 13:34:06 +1000

    fx

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error("Cannot instantiate an abstract class");
-    }
-
-    if (typeof sqft !== "number") {
-      throw new TypeError("sqft must be a number");
-    }
     this._sqft = sqft;
 
     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
@@ -24,18 +17,3 @@ export default class Building {
     throw new Error("Subclasses must implement evacuationWarningMessage()");
   }
 }
-
-class School extends Building {
-  constructor(sqft) {
-    super(sqft);
-  }
-
-  evacuationWarningMessage() {
-    return `Attention! Evacuate the building immediately!`;
-  }
-}
-
-const mySchool = new School(5000);
-console.log(mySchool.sqft);                       // 5000
-console.log(mySchool.evacuationWarningMessage()); // "Attention! Evacuate the building immediately!"
-
