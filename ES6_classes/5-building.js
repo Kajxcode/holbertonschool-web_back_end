b448e5cb2345fbd2ae7b4549c15a64159886adfd
kajxcode <kaj@gmail.com>
2025-09-09 13:24:09 +1000

    fx

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -5,6 +5,16 @@ export default class Building {
         }
         this._sqft = sqft;
     }
+
+    get sqft() {
+        return this._sqft
+    }
+    set sqft(value) {
+        if (new.target === Building) {
+            throw new Error("cannot instantiate an abstract class")
+        }
+        this._sqft = value;
+    }
     evacuationWarningMessage() {
         throw new Error ("Subclasses must implement evacuationWarningMessage()");
     }
