52b299a894b595a01fe3eba0cf5960e806269768
kajxcode <kaj@gmail.com>
2025-09-09 13:36:27 +1000

    fx

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
+    if (this.constructor !== Building
+      && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error("Subclasses must implement evacuationWarningMessage()");
   }
 }
