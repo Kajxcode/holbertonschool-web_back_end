8220f58670a60d8da9f6ec2fef19d2c37fe3a5f8
kajxcode <kaj@gmail.com>
2025-09-16 14:59:43 +1000

    fx

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { createUser, uploadPhoto } from "./utils";
 
 export default async function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-    .then((photo, user) => {
-        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-        console.log(('Signup system offline'));
-    });
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch {
+    console.log('Signup system offline');
+  }
 }
