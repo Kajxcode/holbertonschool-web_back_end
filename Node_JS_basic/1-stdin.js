21dbdfe421582137a36dde4f671cce36bbb96342
kajxcode <kaj@gmail.com>
2025-09-16 22:43:33 +1000

    fx

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// app.js
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Welcome to Holberton School, what is your name?\n", (answer) => {
+  console.log(`Hello, ${answer}!`);
+  rl.close();
+});
