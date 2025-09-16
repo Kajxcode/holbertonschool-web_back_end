f16db04e5f97e78a51443c4bcd71027ea82dc50b
kajxcode <kaj@gmail.com>
2025-09-16 15:38:55 +1000

    fx

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
-import {signUpUser} from "./4-user-promise";
-import {uploadPhoto} from "./5-photo-reject";
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 export default async function handleProfileSignup(firstName, lastName, fileName) {
     const results = await Promise.allSettled([
         signUpUser(firstName, lastName),
         uploadPhoto(fileName)
     ]);
-    
+
     return results.map(res => {
         if (res.status === 'fulfilled') {
             return { status: res.status, value: res.value};
