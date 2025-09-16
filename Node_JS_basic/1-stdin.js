51c4846c92dc2444fb88eed55c0c20aef60d9a03
kajxcode <kaj@gmail.com>
2025-09-16 23:02:43 +1000

    fx

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
-
-process.stdout.write('Welcome to Holberton School, what is your name?\r');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (chunk) => {
-  const name = chunk.trim();
-  process.stdout.write(`Your name is: ${name}\r`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) {
+    const name = input.replace(/[\r\n]+$/, '');
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\r');
+  process.stdout.write('This important software is now closing\n');
 });
