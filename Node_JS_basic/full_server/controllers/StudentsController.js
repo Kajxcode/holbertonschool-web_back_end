c8ec12c34afaccffe68aa8c90cb6c4127eaa3fb7
kajxcode <kaj@gmail.com>
2025-09-19 11:29:15 +1000

    fx

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,23 +1,18 @@
-import { readDatabase } from '../utils';
+import { readDatabase } from '../utils.js';
 
 class StudentsController {
   static getAllStudents(req, res) {
     const databaseFile = process.argv[2];
-
     readDatabase(databaseFile)
       .then((students) => {
-        let responseText = 'This is the list of our students\n';
         const sortedFields = Object.keys(students).sort();
+        const responseText = sortedFields
+          .map((field) => `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`)
+          .join('\n');
 
-        sortedFields.forEach((field) => {
-          responseText += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
-        });
-
-        return res.status(200).send(responseText.trim());
+        res.status(200).send(`This is the list of our students\n${responseText}`);
       })
-      .catch(() => {
-        return res.status(500).send('Cannot load the database');
-      });
+      .catch(() => res.status(500).send('Cannot load the database'));
   }
 
   static getAllStudentsByMajor(req, res) {
@@ -25,19 +20,14 @@ class StudentsController {
     const { major } = req.params;
 
     if (major !== 'CS' && major !== 'SWE') {
-      return res.status(500).send('Major parameter must be CS or SWE');
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
     }
 
-    readDatabase(databaseFile)
-      .then((students) => {
-        if (!students[major]) {
-          return res.status(200).send('List:');
-        }
-        return res.status(200).send(`List: ${students[major].join(', ')}`);
-      })
-      .catch(() => {
-        return res.status(500).send('Cannot load the database');
-      });
+    return readDatabase(databaseFile).then((students) => {
+      const list = students[major] ? students[major].join(', ') : '';
+      res.status(200).send(`List: ${list}`);
+    }).catch(() => res.status(500).send('Cannot load the database'));
   }
 }
 
