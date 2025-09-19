0607c6253139fb8ef660df8de8035b21e861cde7
kajxcode <kaj@gmail.com>
2025-09-19 11:27:54 +1000

    fx

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,6 +3,7 @@ import { readDatabase } from '../utils';
 class StudentsController {
   static getAllStudents(req, res) {
     const databaseFile = process.argv[2];
+
     readDatabase(databaseFile)
       .then((students) => {
         let responseText = 'This is the list of our students\n';
@@ -12,32 +13,30 @@ class StudentsController {
           responseText += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
         });
 
-        res.status(200).send(responseText.trim());
+        return res.status(200).send(responseText.trim());
       })
       .catch(() => {
-        res.status(500).send('Cannot load the database');
+        return res.status(500).send('Cannot load the database');
       });
   }
 
   static getAllStudentsByMajor(req, res) {
     const databaseFile = process.argv[2];
-    const major = req.params.major;
+    const { major } = req.params;
 
     if (major !== 'CS' && major !== 'SWE') {
-      res.status(500).send('Major parameter must be CS or SWE');
-      return;
+      return res.status(500).send('Major parameter must be CS or SWE');
     }
 
     readDatabase(databaseFile)
       .then((students) => {
         if (!students[major]) {
-          res.status(200).send('List:');
-        } else {
-          res.status(200).send(`List: ${students[major].join(', ')}`);
+          return res.status(200).send('List:');
         }
+        return res.status(200).send(`List: ${students[major].join(', ')}`);
       })
-      .catch((err) => {
-        res.status(500).send('Cannot load the database');
+      .catch(() => {
+        return res.status(500).send('Cannot load the database');
       });
   }
 }
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,22 +1,27 @@
-const fs = require('fs');
+import fs from 'fs';
 
 export function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
-        return reject(new Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
+        return;
       }
 
-      const lines = data.trim().split('\n').filter(line => line !== '');
+      const lines = data
+        .trim()
+        .split('\n')
+        .filter((line) => line !== '');
       if (lines.length === 0) {
-        return resolve({});
+        resolve({});
+        return;
       }
 
-      const fields = lines.slice(1); // Exclude the header
       const studentData = {};
+      const records = lines.slice(1); // Exclude the header
 
-      fields.forEach(line => {
-        const [firstname, lastname, age, field] = line.split(',');
+      records.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
         if (!studentData[field]) {
           studentData[field] = [];
         }
@@ -28,4 +33,4 @@ export function readDatabase(filePath) {
   });
 }
 
-export default readDatabase
+export default readDatabase;
